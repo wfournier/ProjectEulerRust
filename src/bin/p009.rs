2058c831133404eee31a4fa866be80639069891a
William Fournier <wfournier2710@gmail.com>
2019-01-11 12:05:23 -0500

    small update

--- src/bin/p009.rs
+++ src/bin/p009.rs
@@ -1,12 +1,20 @@
+extern crate stopwatch;
+use stopwatch::{Stopwatch};
+
 fn main() {
+    let sw = Stopwatch::start_new();
+    
     let (a, b, c, prod) = solve(1_000);
     println!("{} X {} X {} = {}", a, b, c, prod);
+
+    println!("Elapsed: {}ms", sw.elapsed_ms());
 }
 
 fn solve(n: u64) -> (u64, u64, u64, u64) {
     for a in 1..n {
         for b in a + 1..n {
             for c in b + 1..n {
+                // println!("a = {}, b = {}, c = {}", a, b, c);
                 if a.pow(2) + b.pow(2) == c.pow(2) && a + b + c == n {
                     return (a, b, c, a * b * c);
                 }
